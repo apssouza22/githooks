f098ea59a57c5032ffc577bc96ddb4545f9b4aa5
Alexsandro Souza <alexsandro.souza@deem.com>
2020-04-12 23:35:19 +0100

    Add file exists condition to run golangci-lint

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Running pre-commit hook..."
-if [ -x "$(command -v golangci-lint)" ]; then
+
+if [ -x "$(command -v golangci-lint)" ] && [ -f "$PWD"/.golangci.yml  ]; then
   echo "Running Go static code analyzer at $PWD"
   golangci-lint cache clean
   golangci-lint run --new
--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -13,8 +13,8 @@ BRANCH_NAME=$(git symbolic-ref --short HEAD) # Get current branch name
 JIRA_TICKET=$(echo ${BRANCH_NAME} | perl -wlne 'print $1 if /(HOTMOD-[^-]+)/') # extract the Jira ticket
 
 BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
-BRANCH_IN_COMMIT=$(grep -c "\[JIRA_TICKET\]" "$1")
-#
+BRANCH_IN_COMMIT=$(grep -c "$JIRA_TICKET" $1)
+
 if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then
   sed -i.bak -e "1s/^/$JIRA_TICKET /" "$1"
 fi
